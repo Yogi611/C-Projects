b00c10cb2d0ce9065075fe4b5ffc80ed5dae283f
codemedian Yogi <ys71161@gmail.com>
2025-05-24 17:40:06 +0530

    Major Bug Fix

--- fixes.c
+++ fixes.c
@@ -3,5 +3,6 @@ int main(){
     printf("Stop saying 'Hello, World!'\n");
     printf("Nice to meet you, World!'\n");
     printf("or try to not say anything at all!\n");
+    printf("Wave to the World instead!\n");
     return 0;
 }
