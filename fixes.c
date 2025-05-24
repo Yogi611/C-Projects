8d70a14931f1739db8f089a866eeabdc2b3d5ab6
codemedian Yogi <ys71161@gmail.com>
2025-05-24 17:51:49 +0530

    Goodbye world

--- fixes.c
+++ fixes.c
@@ -2,5 +2,7 @@
 int main(){
     printf("Stop saying 'Hello, World!'\n");
     printf("Nice to meet you, World!'\n");
+    printf("or try to not say anything at all!\n");
+    printf("Wave to the World instead!\n");
     return 0;
 }
